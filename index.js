2e25fabd0180e23a0fc3aa02aa8211b73783f9ec
Tim Chin <timchin@Tims-MacBook-Pro.local>
2017-01-23 15:10:41 -0800

    Adds printBoard, package.json, npm prompt, and .gitignore

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+const prompt = require('prompt');
+prompt.start();
+
 // Board
+var board = {
+  1: ' ',
+  2: ' ',
+  3: ' ',
+  4: ' ',
+  5: ' ',
+  6: ' ',
+  7: ' ',
+  8: ' ',
+  9: ' ',
+};
+
+
+// Print board
+var printBoard = function() {
+  var board = Board();
+  console.log(
+    '---------' + '\n' + 
+    '|' + board[1] + '|' + board[2] + '|' + board[3] + '|' + '\n' +
+    '---------' + '\n' + 
+    '|' + board[3] + '|' + board[4] + '|' + board[5] + '|' + '\n' +
+    '---------' + '\n' + 
+    '|' + board[7] + '|' + board[8] + '|' + board[9] + '|' + '\n' +
+    '---------'
+  );
+};
+
+
+// Win condition check
+
