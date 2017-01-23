b3480d3ab349fc8c59ebc401a3b561d9f40dd564
Tim Chin <timchin@Tims-MacBook-Pro.local>
2017-01-23 15:28:47 -0800

    Adds playGame function

--- index.js
+++ index.js
@@ -2,33 +2,75 @@ const prompt = require('prompt');
 prompt.start();
 
 // Board
-var board = {
-  1: ' ',
-  2: ' ',
-  3: ' ',
-  4: ' ',
-  5: ' ',
-  6: ' ',
-  7: ' ',
-  8: ' ',
-  9: ' ',
+var Board = function() {
+  return {
+    1: ' ',
+    2: ' ',
+    3: ' ',
+    4: ' ',
+    5: ' ',
+    6: ' ',
+    7: ' ',
+    8: ' ',
+    9: ' '
+  }
 };
 
 
 // Print board
 var printBoard = function() {
-  var board = Board();
   console.log(
-    '---------' + '\n' + 
     '|' + board[1] + '|' + board[2] + '|' + board[3] + '|' + '\n' +
-    '---------' + '\n' + 
     '|' + board[3] + '|' + board[4] + '|' + board[5] + '|' + '\n' +
-    '---------' + '\n' + 
-    '|' + board[7] + '|' + board[8] + '|' + board[9] + '|' + '\n' +
-    '---------'
+    '|' + board[7] + '|' + board[8] + '|' + board[9] + '|' + '\n'
   );
 };
 
-
 // Win condition check
+var winCondition = function() {
+  let possibles = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+};
+
+var fullBoard = function(board) {
+  for (var prop in board) {
+    
+  }
+}
+
+var playGame = function() {
+  var board = Board();
+  var playerX = true;
+  while (winCondition() === false) {
+    printBoard();
+    console.log("It is Player " + playerNum + "\'s turn.");
+    var getMove = function() {
+      prompt.get(['What space (1-9)?'], function(err, result) {
+        if (err) { return err; }
+        if (playerX) {
+          board[result] = 'X';
+        } else {
+          board[result] = 'O';
+        }
+        if (!board[result]) {
+          console.log('Please enter a move between 1-9');
+          getMove();
+        }
+        if (board[result] !== ' ') {
+          console.log('Space is already taken');
+          getMove();
+        }
+      });
+    };
+  }
+  getMove();
+  
+};
+
+
+
+playGame();
 
