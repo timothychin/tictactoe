b63bdea6fa6c969062893147e1ff591310c950e0
Tim Chin <timchin@Tims-MacBook-Pro.local>
2017-01-23 15:39:07 -0800

    Adds fullBoard and winCondition

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ prompt.start();
 // Board
 var Board = function() {
   return {
-    1: ' ',
-    2: ' ',
-    3: ' ',
-    4: ' ',
-    5: ' ',
-    6: ' ',
-    7: ' ',
-    8: ' ',
-    9: ' '
+    1: '-',
+    2: '-',
+    3: '-',
+    4: '-',
+    5: '-',
+    6: '-',
+    7: '-',
+    8: '-',
+    9: '-'
   }
 };
 
@@ -20,26 +20,41 @@ var Board = function() {
 // Print board
 var printBoard = function() {
   console.log(
-    '|' + board[1] + '|' + board[2] + '|' + board[3] + '|' + '\n' +
-    '|' + board[3] + '|' + board[4] + '|' + board[5] + '|' + '\n' +
-    '|' + board[7] + '|' + board[8] + '|' + board[9] + '|' + '\n'
+    board[1] + board[2] + board[3] + '\n' +
+    board[3] + board[4] + board[5] + '\n' +
+    board[7] + board[8] + board[9] + '\n'
   );
 };
 
 // Win condition check
-var winCondition = function() {
+var winCondition = function(board) {
   let possibles = [
     [1, 2, 3],
     [4, 5, 6],
-    [7, 8, 9]
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
   ];
+  for (var i = 0; i < possibles.length; i++) {
+    if (board[possibles[i][0]] === board[possibles[i][1]] && board[possibles[i][1]] === board[possibles[i][2]]) {
+      var playerNum = board[possibles[0]] === 'X' ? 1 : 2;
+      console.log('Player ' + playerNum + ' wins!');
+    }
+  }
 };
 
 var fullBoard = function(board) {
+  var count = 0;
   for (var prop in board) {
-    
+    if (board[prop] !== '-') {
+      count++;
+    }
   }
-}
+  return count === 9 ? true : false;
+};
 
 var playGame = function() {
   var board = Board();
@@ -65,9 +80,11 @@ var playGame = function() {
         }
       });
     };
+    if (fullBoard(board)) {
+      break;
+    }
   }
   getMove();
-  
 };
 
 
